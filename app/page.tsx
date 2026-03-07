d2f58101ebfbb19edfb4ae09a8d7dc1dd7c1324e
Doniyor <doniyoryusupob246@gmail.com>
2026-03-07 23:00:06 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div className='flex justify-center flex-col mt-10 shadow-lg w-[90%] max-w-[500px] mx-auto items-center p-5 bg-[#00ABC2] text-white rounded-xl'>
         <div className='flex flex-col items-center'>
           <h2 className={` text-3xl md:text-4xl font-bold text-center mb-3`}>Türk Dünyası Gençlik Topluluğu</h2>
-          <p className={`${libreBaskerville.className} text-[18px] md:text-lg text-white leading-relaxed max-w-2xl px-2 md:px-4 text-center`}>Türk Dünyası Gençliğinin bir araya <br /> geldiği yer</p>
+          <p className={`${libreBaskerville.className} text-[15px] md:text-lg text-white leading-relaxed max-w-2xl px-2 md:px-4 text-center`}>Türk Dünyası Gençliğinin bir araya <br /> geldiği yer</p>
         </div>
         <Link className='font-mono font-bold w-[90%] sm:w-[300px] text-center mx-auto mt-5 bg-white text-black px-4 py-3 sm:py-2 rounded-xl' href="https://forms.gle/FxtvTTz9XEbHPnAE6">Şimdi Üyemiz Olun</Link>
       </div>
--- lib/gundem.ts
+++ lib/gundem.ts
@@ -1,26 +1,27 @@
 export const items = [
+
+
+
     {
-        id: '1',
-        title: 'GSB GENÇLİK MERKEZİ ZİYARETİ',
-        img: '/gundem/2.jpg',
-        images: ['/gundem/2.jpg'],
+        id: '4',
+        title: 'T.C. BİLECİK İL KÜLTÜR VE TURİZM MÜDÜRLÜĞÜ ZİYARETİ',
+        img: '/gundem/5.jpg',
+        images: ['/gundem/5.jpg', '/gundem/6.jpg'],
 
         subtitle:
-            'Topluluğumuzun kuruluş heyecanını yaşadığımız 2024 yılında, Bilecik Gençlik ve Spor İl Müdürlüğü bünyesinde faaliyet gösteren Gençlik Merkezi Müdürü Sayın Nihat Kara’yı makamında ziyaret etmiştik. Kuruluş aşamasındaki hedeflerimizi ve hayata geçirmeyi planladığımız projeleri paylaştığımız bu görüşme, topluluğumuzun vizyonunu netleştirmemiz açısından büyük önem taşıyordu. Gençlerin sosyal ve kültürel gelişimine yönelik ortak projeler üzerinde durduğumuz bu verimli ziyarette, Sayın Kara’nın tecrübeleri ve topluluğumuza olan olumlu yaklaşımı bizler için motivasyon kaynağı oldu. Gençliğin enerjisini projelere dönüştürme yolunda bizlere kapılarını açan Sayın Nihat Kara’ya destekleri için teşekkürlerimizi sunarız.',
-        date: '14.03.2025',
-
+            'Bilecik İl Kültür ve Turizm Müdürlüğü görevine yeni atanan Sayın Kürşat BOZKURT, topluluğumuz temsilcileri tarafından makamında ziyaret edildi. Ziyaret kapsamında Sayın BOZKURT’a yeni görevinin hayırlı olması temennilerinde bulunulurken, topluluğumuzun vizyonu ve bugüne kadar gerçekleştirdiği çalışmalar hakkında detaylı bilgiler paylaşıldı. Görüşmede, Bilecik’in kültürel mirasını korumak adına topluluğumuz ile İl Kültür ve Turizm Müdürlüğü arasında yapılabilecek ortak projeler üzerine verimli bir istişare gerçekleştirildi. Nazik kabulleri ve misafirperverlikleri için Sayın Kürşat BOZKURT’a teşekkür eder, yeni görevinde başarılar dileriz.',
+        date: '14.01.2026',
     },
 
     {
-        id: '2',
-        title: "BİLECİK ŞEYH EDEBALİ ÜNİVERSİTESİ REKTÖR YARDIMICI MURAT YURDAKUL'A ZİYARET",
-        img: '/gundem/4.jpg',
-        images: ['/gundem/4.jpg'],
+        id: '5',
+        title: 'GENÇLİK VE SPOR BAKAN YARDIMCISI SAYIN ENES EMİNOĞLU İLE ÜNİDES BULUŞMASI',
+        img: '/gundem/7.jpg',
+        images: ['/gundem/7.jpg'],
 
         subtitle:
-            'Üniversitemiz bünyesinde yeni dönemin başlamasıyla birlikte, Türk Dünyası Gençlik Topluluğu Danışmanı Muharrem ÇEVİK ve yönetim ekibi olarak Rektör Yardımcımız Sayın Murat YURDAKUL’u makamında ziyaret ettik.Topluluğumuzun akademik yıl boyunca gerçekleştirmeyi planladığı faaliyet takvimini paylaştığımız bu görüşmede, aynı zamanda Genel Merkezimizin büyük bir emekle hazırladığı Faaliyet Dergimizi kendisine takdim ettik. Dergimizi büyük bir ilgiyle inceleyen Sayın Yurdakul, çalışmalarımızı takdirle karşılayarak topluluğumuza tam destek vereceklerini ifade etti. Üniversite yönetimimizin bu yapıcı yaklaşımı ve desteği, önümüzdeki dönem projelerimiz için bizlere büyük bir motivasyon sağladı. Nazik kabulleri ve yol gösterici tavsiyeleri için Sayın Murat YURDAKUL’a teşekkür ederiz.',
-        date: '23.10.2025',
-
+            "​Türk Dünyası Gençlik Topluluğu olarak, Gençlik ve Spor Bakanlığı tarafından hayata geçirilen ÜNİDES (Üniversite Öğrenci Toplulukları İşbirliği ve Destekleme Programı) projesi kapsamında üniversitemizi ziyaret eden Bakan Yardımcımız Sayın Enes EMİNOĞLU’nu ağırlamanın heyecanını yaşadık. ​Gençlik ve Spor Bakanlığı'nın üniversite topluluklarına sunduğu imkanları ve gençlik odaklı yeni nesil projeleri içeren ÜNİDES projesinin tanıtımı ve istişaresi amacıyla gerçekleştirilen ziyarete, topluluğumuzun yönetim kurulu ve üyeleriyle birlikte tam kadro katılım sağladık. ​Ziyaret esnasında Sayın EMİNOĞLU, devletimizin üniversiteli gençlere ve topluluk faaliyetlerine verdiği önemi vurgulayarak, ÜNİDES projesinin toplulukların kurumsal kapasitelerini nasıl artıracağına dair değerli bilgiler paylaştı.",
+        date: '08.12.2025',
     },
 
     {
@@ -34,16 +35,28 @@ export const items = [
         date: '07.11.2025',
 
     },
+
     {
-        id: '4',
-        title: 'T.C. BİLECİK KÜLTÜR VE TURİZM MÜDÜRLÜĞÜ ZİYARETİ',
-        img: '/gundem/5.jpg',
-        images: ['/gundem/5.jpg', '/gundem/6.jpg'],
+        id: '2',
+        title: "BİLECİK ŞEYH EDEBALİ ÜNİVERSİTESİ REKTÖR YARDIMICISI MURAT YURDAKUL'A ZİYARET",
+        img: '/gundem/4.jpg',
+        images: ['/gundem/4.jpg'],
 
         subtitle:
-            'Bilecik İl Kültür ve Turizm Müdürlüğü görevine yeni atanan Sayın Kürşat Bozkurt, topluluğumuz temsilcileri tarafından makamında ziyaret edildi. Ziyaret kapsamında Sayın Bozkurt’a yeni görevinin hayırlı olması temennilerinde bulunulurken, topluluğumuzun vizyonu ve bugüne kadar gerçekleştirdiği çalışmalar hakkında detaylı bilgiler paylaşıldı. Görüşmede, Bilecik’in kültürel mirasını korumak ve turizm potansiyelini artırmak adına topluluğumuz ile İl Kültür ve Turizm Müdürlüğü arasında yapılabilecek ortak projeler üzerine verimli bir istişare gerçekleştirildi. Nazik kabulleri ve misafirperverlikleri için Sayın Kürşat Bozkurt’a teşekkür eder, yeni görevinde başarılar dileriz.',
-        date: '14.01.2026',
-    },
+            'Üniversitemizin yeni dönemin başlamasıyla birlikte, Türk Dünyası Gençlik Topluluğu Danışmanı Muharrem ÇEVİK ve yönetim ekibi olarak Rektör Yardımcımız Sayın Murat YURDAKUL’u makamında ziyaret ettik.Topluluğumuzun akademik yıl boyunca gerçekleştirmeyi planladığı faaliyet takvimini paylaştığımız bu görüşmede, aynı zamanda Genel Merkezimizin büyük bir emekle hazırladığı Faaliyet Dergimizi kendisine takdim ettik. Dergimizi büyük bir ilgiyle inceleyen Sayın YURDAKUL, çalışmalarımızı takdirle karşılayarak topluluğumuza tam destek vereceklerini ifade etti. Üniversite yönetimimizin bu yapıcı yaklaşımı ve desteği, önümüzdeki dönem projelerimiz için bizlere büyük bir motivasyon sağladı. Nazik kabulleri ve yol gösterici tavsiyeleri için Sayın Murat YURDAKUL’a teşekkür ederiz.',
+        date: '23.10.2025',
 
+    },
+    {
+        id: '1',
+        title: 'GSB GENÇLİK MERKEZİ ZİYARETİ',
+        img: '/gundem/2.jpg',
+        images: ['/gundem/2.jpg'],
+
+        subtitle:
+            'Topluluğumuzun kuruluş heyecanını yaşadığımız 2024 yılında, Bilecik Gençlik ve Spor İl Müdürlüğü bünyesinde faaliyet gösteren Gençlik Merkezi Müdürü Sayın Nihat KA    RA’yı makamında ziyaret etmiştik. Kuruluş aşamasındaki hedeflerimizi ve hayata geçirmeyi planladığımız projeleri paylaştığımız bu görüşme, topluluğumuzun vizyonunu netleştirmemiz açısından büyük önem taşıyordu. Gençlerin sosyal ve kültürel gelişimine yönelik ortak projeler üzerinde durduğumuz bu verimli ziyarette, Sayın KARA’nın tecrübeleri ve topluluğumuza olan olumlu yaklaşımı bizler için motivasyon kaynağı oldu. Gençliğin enerjisini projelere dönüştürme yolunda bizlere kapılarını açan Sayın Nihat KARA’ya destekleri için teşekkürlerimizi sunarız.',
+        date: '14.03.2025',
+
+    },
 
 ];
